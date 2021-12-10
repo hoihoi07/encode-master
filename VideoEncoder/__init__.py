5a26bc6babe2aaca26b1d9895732376b31c99c19
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-10 19:57:19 +0530

    Update __init__.py

--- VideoEncoder/__init__.py
+++ VideoEncoder/__init__.py
@@ -73,7 +73,7 @@ PROGRESS = """
 
 🕰 **ETA:** `{4}`
 """
-"""
+
 if not os.path.isdir(download_dir):
     os.makedirs(download_dir)
 
@@ -106,4 +106,4 @@ app = Client(
     api_hash=api_hash,
     plugins={'root': os.path.join(__package__, 'plugins')},
     parse_mode="html",
-    sleep_threshold=30)
+    sleep_threshold=12)
