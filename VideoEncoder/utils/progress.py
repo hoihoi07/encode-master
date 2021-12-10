133fd424ce7afb64025f2147442ef77f56ef2efd
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-10 19:33:00 +0530

    Update progress.py

--- VideoEncoder/utils/progress.py
+++ VideoEncoder/utils/progress.py
@@ -33,8 +33,8 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
         progress = "[{0}{1}]".format(
-            ''.join(["█" for i in range(math.floor(percentage / 10))]),
-            ''.join(["░" for i in range(10 - math.floor(percentage / 10))])
+            ''.join(["◾" for i in range(math.floor(percentage / 10))]),
+            ''.join(["◽" for i in range(10 - math.floor(percentage / 10))])
         )
         tmp = progress + PROGRESS.format(
             humanbytes(current),
