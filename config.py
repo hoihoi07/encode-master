b9ee01e9ea563920dd77edc6eb597c129bbfb990
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-11 15:02:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
+import logging
+import os
+   
+class Config(object):
+    LOGGER = logging
+    OWNER_ID = int(os.environ.get("OWNER_ID", 1445283714))
+    PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
+    PRO_USERS.append(OWNER_ID)
+    MONGODB_URI = os.environ.get("MONGODB_URI", "")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100"))
+    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
 
