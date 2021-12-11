ba160a5b76431c4f11a5a68d51e84df54e91af3b
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-11 15:07:27 +0530

    Update commands.py

--- VideoEncoder/plugins/commands.py
+++ VideoEncoder/plugins/commands.py
@@ -3,8 +3,11 @@ from translation import Translation
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+
+
 @Client.on_message(filters.command(["start"]) & filters.private)
 async def start(bot, update):
+    await add_user_to_database(bot, update)
     await update.reply_text(
         text=Translation.START_TEXT.format(update.from_user.mention),
         disable_web_page_preview=True,
