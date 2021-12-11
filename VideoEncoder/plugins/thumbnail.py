15da615eeb15313924d5d8f7791c499270701f7e
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-11 19:52:22 +0530

    Update thumbnail.py

--- VideoEncoder/plugins/thumbnail.py
+++ VideoEncoder/plugins/thumbnail.py
@@ -26,7 +26,7 @@ from ..utils.utils import convert_to_jpg, get_file_mimetype
 
 @Client.on_message(filters.command('sthumb'))
 async def savethumbnail(client, message):
-    await add_user_to_database(bot, update)
+    await add_user_to_database(client, message)
     reply = message.reply_to_message
     document = message.document
     photo = message.photo
@@ -62,7 +62,7 @@ async def savethumbnail(client, message):
 
 @Client.on_message(filters.command('dthumb'))
 async def rmthumbnail(client, message):
-    await add_user_to_database(bot, update)
+    await add_user_to_database(client, message)
     for path in ('thumbnail'):
         path = os.path.join(str(message.from_user.id), f'{path}.jpg')
         if os.path.isfile(path):
