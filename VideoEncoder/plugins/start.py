4011216210d206dd6d2ee060ffa2de6820ffa3c6
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-11 19:26:31 +0530

    Update start.py

--- VideoEncoder/plugins/start.py
+++ VideoEncoder/plugins/start.py
@@ -24,11 +24,8 @@ from ..utils.utils import output
 
 
 
-@Client.on_message(filters.command('vset'))
+@Client.on_message(filters.command('settings'))
 async def vset(app, message):
-    check = await check_user(message)
-    if check is None:
-        return
     text = f'''<b>Encode Settings</b>
 Tune: <code>{tune}</code> | <code>Preset: {preset}</code>
 Audio: <code>{audio} | <code>CRF: {crf}</code>
@@ -38,16 +35,11 @@ Resolution: <code>{resolution}</code>
 Upload Mode: <code>{'Document' if (upload_doc) else 'Video' }</code>
 Doc thumb: <code>{'True' if (doc_thumb) else 'False'}</code>
 
-<b>Sudo Users</b>
-<code>{sudo_users}</code>
 '''
     await message.reply(text=text, reply_markup=start)
 
 
 @Client.on_message(filters.command('logs'))
 async def logs(app, message):
-    check = await check_user(message)
-    if check is None:
-        return
     file = 'VideoEncoder/utils/logs.txt'
     await message.reply_document(file, caption='#Logs')
