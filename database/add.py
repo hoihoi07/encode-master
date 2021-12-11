6c3d26dd5031b2029252e077ab436e582a96dc80
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-11 15:11:08 +0530

    Update add.py

--- database/add.py
+++ database/add.py
@@ -1,1 +1,13 @@
+# (c) @AbirHasan2005
+
+from config import Config
+from database.database import db
+from pyrogram import Client
+from pyrogram.types import Message
+
+
+async def add_user_to_database(bot: Client, update: Message):
+    if not await db.is_user_exist(update.from_user.id):
+           await db.add_user(update.from_user.id)
+
 
